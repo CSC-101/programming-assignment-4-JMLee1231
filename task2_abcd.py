852ff54a46fed6c384472eebb9b9616c60945fca
JMLee1231 <jlee1231@calpoly.edu>
2024-11-29 08:24:31 -0800

    Update task2_abcd.py
    60 -> 40

--- task2_abcd.py
+++ task2_abcd.py
@@ -10,12 +10,12 @@ percent:Income.Persons Below Poverty Level
 
 #contents of file 3
 
-filter-gt:Ethnicities.Asian Alone:60
+filter-gt:Ethnicities.Asian Alone:40
 percent:Income.Persons Below Poverty Level
 
 #contents of file 4
 
-filter-lt:Ethnicities.Asian Alone:60
+filter-lt:Ethnicities.Asian Alone:40
 percent:Income.Persons Below Poverty Level
 
 #should be split probably
