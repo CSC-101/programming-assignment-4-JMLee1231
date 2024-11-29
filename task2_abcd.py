f700c1ac476b7bf5cd90d47b260542bc372ab048
JMLee1231 <jlee1231@calpoly.edu>
2024-11-29 07:46:48 -0800

    Update and rename hw4v2.py to task2_abcd.py

---
+++ task2_abcd.py
@@ -0,0 +1,19 @@
+#contents of file 1
+
+filter-lt:Education.High School or Higher:80
+percent:Income.Persons Below Poverty Level
+
+#contents of file 2
+
+filter-gt:Education.Bachelor's Degree or Higher:40
+percent:Income.Persons Below Poverty Level
+
+#contents of file 3
+
+filter-gt:Ethnicities.Asian Alone:60
+percent:Income.Persons Below Poverty Level
+
+#contents of file 4
+
+filter-lt:Ethnicities.Asian Alone:60
+percent:Income.Persons Below Poverty Level
